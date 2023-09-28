a247b727655b82aebb4f10356da9c28287fc870d
Ferdy Wahyudi <ferdyw6@gmail.com>
2023-09-28 11:44:53 +0700

    initial commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong> &copy; 2022 <a href="#">Absensi Karyawan ArbooEvolet</a>.</strong>
+</footer>
